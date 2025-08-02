aa12dbf8f72fd2bba2ad95fcbb277088824dc878
LJ-44 <lucasj040505@gmail.com>
2025-08-02 17:16:00 -0500

    gui

--- run_game.py
+++ run_game.py
@@ -1,19 +1,28 @@
 import pygame
 import pygame_gui as GUI
+from pygame_gui.elements import UIButton, UITextBox
+from pygame.locals import *
 
 pygame.init()
 
 pygame.display.set_caption('Quick Start')
+monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 screen = pygame.display.set_mode((800, 600))
 
 background = pygame.Surface((800, 600))
 background.fill(pygame.Color("#000000"))
 
-manager = GUI.UIManager((800, 600))
+manager = GUI.UIManager((800, 600), theme_path="data/themes/bullet_hell_theme.json")
+manager.add_font_paths(font_name="Bullet_Hell", regular_path="assets/font/BulletHell_font.ttf")
 
-button = GUI.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100,50)),
+button = UIButton(relative_rect=pygame.Rect((350, 275), (100,50)),
                                text="Hello",
-                               manager=manager)
+                               manager=manager,)
+title = UITextBox(relative_rect=pygame.Rect((250, 175), (100,50)), 
+                  html_text="BULLET HELL", 
+                  manager=manager)
+
+title.set_active_effect(GUI.TEXT_EFFECT_FADE_IN)
 
 clock = pygame.time.Clock()
 running = True
